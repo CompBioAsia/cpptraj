95f10adcadf7b64eb015a9e4473d0d9e38273780
hughests <travishughes9@gmail.com>
2026-06-19 11:09:11 +0800

    Add files via upload

---
+++ movie.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+prmtop="abl_ligand.prmtop"
+ref="step9.rst7"
+name="abl_lig" #name of trajectory file (.nc file)
+res="1-265"          #number of residues in system
+
+#Remove old files
+rm -f cpptraj-movie.sh $name-movie.cpptraj
+
+#Create cpptraj input file
+cat > "$name-movie.cpptraj" <<EOF
+parm $prmtop
+reference $ref
+trajin $name.nc
+
+autoimage #centers and images the trajectory
+rms Autofit :$res@CA reference mass #calculate a mass weighted, RMS fit to the 1st structure
+
+strip :WAT
+strip :Na+
+
+trajout ${name}-movie.nc
+trajout trajectory.pdb pdb onlyframes 1
+
+go
+quit
+EOF
+
+#Create shell script to run cpptraj
+cat > cpptraj-movie.sh <<EOF
+#!/bin/bash
+cpptraj -i $name-movie.cpptraj
+EOF
+
+chmod +x cpptraj-movie.sh
+./cpptraj-movie.sh
+
+exit 0
---
+++ rmsd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export PATH=/data/tools/ambertools25/bin:$PATH
+offset=1
+ref=abl_ligand.inpcrd
+prmtop=abl_ligand.prmtop
+
+# Clean start
+rm -f rmsd.cpptraj
+
+# Write cpptraj input
+{
+  echo "parm $prmtop"
+  echo "trajin abl_lig.nc 1 last $offset"
+  echo "reference $ref parm $prmtop [$ref]"
+  echo "autoimage"
+  echo "rms @CA ref [$ref] out $ref-rmsd-pre.csv mass"
+  echo "rms :265 ref [$ref] out $ref-rmsd-ligpre.csv mass nofit"
+  echo "go"
+  echo "quit"
+} >> rmsd.cpptraj
+
+# Run cpptraj
+cpptraj -i rmsd.cpptraj
+# add actual commas to the .csv file so the frame number and rmsd are in seperate columns in excel
+
+sed 's/^ *//; s/ \+/,/g' $ref-rmsd-pre.csv > ablrmsd.csv
+rm $ref-rmsd-pre.csv
+
+sed 's/^ *//; s/ \+/,/g' $ref-rmsd-ligpre.csv > ligrmsd.csv
+rm $ref-rmsd-ligpre.csv
